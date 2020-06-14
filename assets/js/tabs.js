07f57fff1ac93ff9c99dce101df03a86ea0e962e
YuliaKalina <ukalinicenko15@gmail.com>
2020-06-14 18:00:50 +0300

    Added tabs

---
+++ assets/js/tabs.js
@@ -0,0 +1,442 @@
+//
+// <!--  <div class="container  row ml-0">-->
+// <!--    <div class=" my-5">-->
+// <!--      <div class="tab-content ">-->
+// <!--        <div id="home" class="tab-pane active">-->
+// <!--          <div class="col-lg-12  col-5 offset-lg-2 offset-1 my-5">-->
+// <!--            <h3 class="text-dark">Тариф 50</h3>-->
+// <!--          </div>-->
+// <!--          <div class="col-12 offset-lg-2 ">-->
+// <!--            <table class="table">-->
+// <!--              <thead>-->
+// <!--              <tr>-->
+// <!--                <th scope="col">#</th>-->
+//     <!--                <th scope="col">Услуга</th>-->
+//     <!--                <th scope="col">Цена</th>-->
+//     <!--              </tr>-->
+//     <!--              </thead>-->
+//     <!--              <tbody>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">1</th>-->
+//     <!--                <td>Подключение</td>-->
+//     <!--                <td>Бесплатное</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">2</th>-->
+//     <!--                <td>Абонентская плата</td>-->
+// <!--                <td>100</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">3</th>-->
+//     <!--                <td>Скорость вх./исх., до (Мбит/сек.)</td>-->
+// <!--                <td>10/10</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">4</th>-->
+//     <!--                <td>Объем трафика</td>-->
+// <!--                <td>неограничен-->
+// <!--                </td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">5</th>-->
+//     <!--                <td>Круглосуточная служба поддержки</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">6</th>-->
+//     <!--                <td> Настрой кино</td>-->
+// <!--                <td>50</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">7</th>-->
+//     <!--                <td>Ночной</td>-->
+//     <!--                <td>50</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">8</th>-->
+//     <!--                <td> Онлайн-кинотеатр IVI</td>-->
+// <!--                <td>30</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">9</th>-->
+//     <!--                <td> Онлайн-кинотеатр MEGOGO</td>-->
+// <!--                <td>35</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">10</th>-->
+//     <!--                <td> Реальный IP адрес</td>-->
+// <!--                <td>40</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">11</th>-->
+//     <!--                <td> SMS уведомление</td>-->
+// <!--                <td>5</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">12</th>-->
+//     <!--                <td> Подписка на антивирусы</td>-->
+// <!--                <td>20</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">13</th>-->
+//     <!--                <td> WiFi роутер</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">14</th>-->
+//     <!--                <td> Настройка WiFi</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">15</th>-->
+//     <!--                <td>Добровольная блокировка</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">16</th>-->
+//     <!--                <td>Телевидение + КИНО</td>-->
+//     <!--                <td>300</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">17</th>-->
+//     <!--                <td> Телевидение 277 каналов</td>-->
+// <!--                <td>250</td>-->
+// <!--              </tr>-->
+// <!--              </tbody>-->
+// <!--            </table>-->
+// <!--          </div>-->
+// <!--        </div>-->
+//
+// <!--        <div id="menu1" class="tab-pane fade">-->
+// <!--          <div class="col-lg-5 col-5 offset-lg-2 offset-1 my-5">-->
+// <!--            <h3>Тариф 100</h3>-->
+// <!--          </div>-->
+// <!--          <div class="col-lg-12  offset-lg-2 ">-->
+// <!--            <table class="table">-->
+// <!--              <thead>-->
+// <!--              <tr>-->
+// <!--                <th scope="col">#</th>-->
+//     <!--                <th scope="col">Услуга</th>-->
+//     <!--                <th scope="col">Цена</th>-->
+//     <!--              </tr>-->
+//     <!--              </thead>-->
+//     <!--              <tbody>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">1</th>-->
+//     <!--                <td>Подключение</td>-->
+//     <!--                <td>400</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">2</th>-->
+//     <!--                <td>Абонентская плата</td>-->
+// <!--                <td>100</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">3</th>-->
+//     <!--                <td>Скорость вх./исх., до (Мбит/сек.)</td>-->
+// <!--                <td>10/10</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">4</th>-->
+//     <!--                <td>Объем трафика</td>-->
+// <!--                <td>неограничен-->
+// <!--                </td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">5</th>-->
+//     <!--                <td>Круглосуточная служба поддержки</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">6</th>-->
+//     <!--                <td> Настрой кино</td>-->
+// <!--                <td>50</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">7</th>-->
+//     <!--                <td>Ночной</td>-->
+//     <!--                <td>50</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">8</th>-->
+//     <!--                <td> Онлайн-кинотеатр IVI</td>-->
+// <!--                <td>30</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">9</th>-->
+//     <!--                <td> Онлайн-кинотеатр MEGOGO</td>-->
+// <!--                <td>35</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">10</th>-->
+//     <!--                <td> Реальный IP адрес</td>-->
+// <!--                <td>40</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">11</th>-->
+//     <!--                <td> SMS уведомление</td>-->
+// <!--                <td>5</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">12</th>-->
+//     <!--                <td> Подписка на антивирусы</td>-->
+// <!--                <td>20</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">13</th>-->
+//     <!--                <td> WiFi роутер</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">14</th>-->
+//     <!--                <td> Настройка WiFi</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">15</th>-->
+//     <!--                <td>Добровольная блокировка</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">16</th>-->
+//     <!--                <td>Телевидение + КИНО</td>-->
+//     <!--                <td>300</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">17</th>-->
+//     <!--                <td> Телевидение 277 каналов</td>-->
+// <!--                <td>250</td>-->
+// <!--              </tr>-->
+// <!--              </tbody>-->
+// <!--            </table>-->
+// <!--          </div>-->
+// <!--        </div>-->
+// <!--        <div id="menu2" class="tab-pane fade">-->
+// <!--          <div class="col-lg-5 col-5 offset-lg-2 offset-1 my-5">-->
+// <!--            <h3>Тариф 200</h3>-->
+// <!--          </div>-->
+// <!--          <div class="col-lg-12 offset-lg-2 ">-->
+// <!--            <table class="table">-->
+// <!--              <thead>-->
+// <!--              <tr>-->
+// <!--                <th scope="col">#</th>-->
+//     <!--                <th scope="col">Услуга</th>-->
+//     <!--                <th scope="col">Цена</th>-->
+//     <!--              </tr>-->
+//     <!--              </thead>-->
+//     <!--              <tbody>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">1</th>-->
+//     <!--                <td>Подключение</td>-->
+//     <!--                <td>500</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">2</th>-->
+//     <!--                <td>Абонентская плата</td>-->
+// <!--                <td>100</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">3</th>-->
+//     <!--                <td>Скорость вх./исх., до (Мбит/сек.)</td>-->
+// <!--                <td>10/10</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">4</th>-->
+//     <!--                <td>Объем трафика</td>-->
+// <!--                <td>неограничен-->
+// <!--                </td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">5</th>-->
+//     <!--                <td>Круглосуточная служба поддержки</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">6</th>-->
+//     <!--                <td> Настрой кино</td>-->
+// <!--                <td>50</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">7</th>-->
+//     <!--                <td>Ночной</td>-->
+//     <!--                <td>50</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">8</th>-->
+//     <!--                <td> Онлайн-кинотеатр IVI</td>-->
+// <!--                <td>30</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">9</th>-->
+//     <!--                <td> Онлайн-кинотеатр MEGOGO</td>-->
+// <!--                <td>35</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">10</th>-->
+//     <!--                <td> Реальный IP адрес</td>-->
+// <!--                <td>40</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">11</th>-->
+//     <!--                <td> SMS уведомление</td>-->
+// <!--                <td>5</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">12</th>-->
+//     <!--                <td> Подписка на антивирусы</td>-->
+// <!--                <td>20</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">13</th>-->
+//     <!--                <td> WiFi роутер</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">14</th>-->
+//     <!--                <td> Настройка WiFi</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">15</th>-->
+//     <!--                <td>Добровольная блокировка</td>-->
+// <!--                <td>0</td>-->
+// <!--              </tr>-->
+// <!--              <tr>-->
+// <!--                <th scope="row">16</th>-->
+//     <!--                <td>Телевидение + КИНО</td>-->
+//     <!--                <td>300</td>-->
+//     <!--              </tr>-->
+//     <!--              <tr>-->
+//     <!--                <th scope="row">17</th>-->
+//     <!--                <td> Телевидение 277 каналов</td>-->
+// <!--                <td>250</td>-->
+// <!--              </tr>-->
+// <!--              </tbody>-->
+// <!--            </table>-->
+// <!--          </div>-->
+// <!--        </div>-->
+// <!--      </div>-->
+// <!--    </div>-->
+//
+//
+// <!--  <div class="col-4 offset-1 mt-5">-->
+// <!--    <ul class="nav nav-tabs">-->
+// <!--      <li class="active"><a data-toggle="tab" class="text-decoration-none" href="#home">-->
+// <!--        <div class="shadow bg-yellow size align-items-center justify-content-center d-flex my-3 offset-4">-->
+// <!--          <p class=" text-dark text-center align-self-center font-weight-bold">Тариф 50</p>-->
+// <!--        </div>-->
+// <!--      </a></li>-->
+// <!--      <li><a data-toggle="tab" class="text-decoration-none" href="#menu1">-->
+//     <!--        <div class="shadow bg-light size align-items-center justify-content-center d-flex">-->
+//     <!--          <p class=" text-dark text-center font-weight-bold">Тариф 100</p>-->
+// <!--        </div>-->
+// <!--      </a></li>-->
+// <!--      <li><a class="text-decoration-none" data-toggle="tab" href="#menu2">-->
+//     <!--        <div class="shadow bg-yellow size align-items-center justify-content-center d-flex my-3 offset-4">-->
+//     <!--          <p class="text-dark text-center font-weight-bold v-align">Тариф 200</p>-->
+// <!--        </div>-->
+// <!--      </a></li>-->
+// <!--    </ul>-->
+// <!--  </div>-->
+// <!--</div>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Footer
+// <div class="col-lg-8">
+//     <div class="m-5">
+//     <h1 class="text-uppercase font-weight-bold caption">IP-<br>Link</h1>
+//     </div>
+//     <div class=" m-5">
+//     <p class="text-uppercase text-secondary">
+//     стабильность, качество, скорость
+// </p>
+// <p class=" text-secondary">
+//     Созданный группой интузиастов для себя и своих соседей, IP-Link считается гарантией быстрого и надежного
+// интернета по доступной цене. <br>
+// Более 100 тысяч пользователей Харьковской области пользуются IP-Link и рекомендуют
+// его своим соседям и знакомым.
+// </p>
+// </div>
+//
+// <div class="m-5">
+//     <a href="#">
+//     <button type="button" class="btn btn-link text-secondary text-decoration-none text-underline">Узнать больше
+// </button>
+// </a>
+// </div>
+// </div>
+//
+//
+// <div class="row">
+//
+//     <div class="row">
+//     <div class="col-lg-1 p-0 m-0">
+//     <img class="p-4 ml-3 logo-width" src="./assets/img/ip-link-logo-my.svg" alt="logo">
+//     <div class="mt-5 ml-3">
+//     <p class="rotate">Техническая и информационная поддержка: 095-947-41-61 | 096-699-11-82</p>
+// </div>
+// </div>
+// <div class="col-lg-5 p-0">
+//     <div class="d-lg-block d-none">
+//     <img class="image-main p-0 m-0" src="assets/img/connection_ps_crop.jpg" alt="connection">
+//     </div>
+//     </div>
+//
+//     <div class="col-lg-6 p-0 m-0">
+//     <nav class="nav nav-pills nav-justified">
+//     <a class="nav-item nav-link p-4 text-secondary bg-yellow" href="#">Услуги</a>
+//     <a class="nav-item nav-link p-4 text-secondary bg-yellow" href="#">Тарифы</a>
+//     <a class="nav-item nav-link p-4 text-secondary bg-light" href="#">Абоненту</a>
+//     </nav>
+//     <div class="col-lg-8">
+//     <div class="m-5">
+//     <h1 class="text-uppercase font-weight-bold caption">IP-<br>Link</h1>
+//     </div>
+//     <div class=" m-5">
+//     <p class="text-uppercase text-secondary">
+//     стабильность, качество, скорость
+// </p>
+// <p class=" text-secondary">
+//     Созданный группой интузиастов для себя и своих соседей, IP-Link считается гарантией быстрого и надежного
+// интернета по доступной цене. <br>
+// Более 100 тысяч пользователей Харьковской области пользуются IP-Link и рекомендуют
+// его своим соседям и знакомым.
+// </p>
+// </div>
+//
+// <div class="m-5">
+//     <a href="#">
+//     <button type="button" class="btn btn-link text-secondary text-decoration-none text-underline">Узнать больше
+// </button>
+// </a>
+// </div>
+// </div>
+// </div>
+// </div>
+// </div>
+// CSS only
